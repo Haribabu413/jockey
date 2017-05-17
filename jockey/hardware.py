d7b49a055f27731a4940966a2b00975fb997ab5e
Jason R. Jones <slightlynybbled@gmail.com>
2017-05-17 12:09:59 -0400

    Instantiating tests in the Application class

---
+++ jockey/hardware.py
@@ -0,0 +1,6 @@
+import subdue
+
+
+def write_daq_ao(voltage, output, save=False, save_column_header=None, override=False):
+    print('writing to daq {}: {}'.format(output, voltage))
+    pass
---
+++ jockey/user/hardware.py
@@ -0,0 +1,9 @@
+import jockey.hardware
+import jockey.user.app as app
+
+
+def write_daq_ao(voltage, output, save=False, save_column_header=None, override=False):
+    print('writing to daq {}: {}'.format(output, voltage))
+    pass
+
+
